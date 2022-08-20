921ec2a0c7a036f1c17d37edeae7df5d893dcab3
saracodic <sarabavifard@gmail.com>
2022-08-20 03:49:00 -0700

    Create reverse_a_number.js
    How to reverse a number with js function?

---
+++ reverse_a_number.js
@@ -0,0 +1,22 @@
+// reverse a number with js function
+
+ // create function with n parameter
+ 
+function reverse_a_number(n)   
+{
+
+//convert number to string 
+
+	n = n + "";      
+  
+/* split string object into an array of strings by separating the string into substrings and  reverse an array in place with reverce method  after that 
+join all elements of an array into a string with join method
+*/
+  
+	return n.split("").reverse().join("");   
+   
+}
+
+// converted string to numbers using the Number() function and log it
+
+console.log(Number(reverse_a_number(15604)));
